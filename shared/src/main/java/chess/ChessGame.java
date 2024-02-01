4a9f1714a47299556dc164c1b64aeb3cfb7dba8d
Jens Shumway <jensshum@gmail.com>
2024-02-01 14:05:17 -0700

    More tests passed

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -54,64 +54,38 @@ public class ChessGame {
      * startPosition
      */
     public Collection<ChessMove> validMoves(ChessPosition startPosition) {
-        Collection<ChessMove> validMoves = null;
-//        System.out.println(myBoard.getPiece(startPosition));
-
-//        if (myBoard.getPiece(startPosition) != null) {
-//            validMoves = myBoard.getPiece(startPosition).pieceMoves(myBoard, startPosition);
-//        }
-
         Collection<ChessMove> playableMoves = new HashSet<>();
-        for (int i = 1; i < 9; i++) {
-            for (int j = 1; j < 9; j++) {
-                ChessPosition myPosition = new ChessPosition(i,j);
-                ChessPiece myPiece = myBoard.getPiece(myPosition);
-                if (myPiece != null && myPiece.getPieceType() != KING) {
-                    if (myPiece.getTeamColor() == TeamColor.BLACK) {
-                        //Get the moves for each piece
-                        Collection<ChessMove> potentialMoves = myPiece.pieceMoves(myBoard, myPosition);
-                        // Move the Piece to each potential position
-                        for (ChessMove move : potentialMoves) {
-                            ChessBoard clonedBoard = myBoard.clone();
-                            try {
-                                makeClonedMove(move, clonedBoard);
-                                if (!isInCheck(TeamColor.BLACK)) {
-                                    playableMoves.add(move);
-                                }
-                            } catch (InvalidMoveException e) {
-                                throw new RuntimeException(e);
-                            }
+        ChessPiece myPiece = myBoard.getPiece(startPosition);
+        if (myPiece != null) {
+            if (myPiece.getTeamColor() == TeamColor.BLACK) {
+                Collection<ChessMove> potentialMoves = myPiece.pieceMoves(myBoard, startPosition);
+                for (ChessMove move : potentialMoves) {
+                    ChessBoard clonedBoard = myBoard.clone();
+                    try {
+                        makeClonedMove(move, clonedBoard);
+                        if (!cloneIsInCheck(TeamColor.BLACK, clonedBoard)) {
+                            playableMoves.add(move);
                         }
+                    } catch (InvalidMoveException e) {
+                        throw new RuntimeException(e);
                     }
-                    else  {
-                        //Get the moves for each piece
-                        Collection<ChessMove> potentialMoves = myPiece.pieceMoves(myBoard, myPosition);
-                        // Move the Piece to each potential position
-                        for (ChessMove move : potentialMoves) {
-                            ChessBoard clonedBoard = myBoard.clone();
-                            try {
-                                makeClonedMove(move, clonedBoard);
-                                if (!isInCheck(TeamColor.WHITE)) {
-                                    playableMoves.add(move);
-                                }
-                            } catch (InvalidMoveException e) {
-                                throw new RuntimeException(e);
-                            }
+                }
+            }
+            else  {
+                Collection<ChessMove> potentialMoves = myPiece.pieceMoves(myBoard, startPosition);
+                for (ChessMove move : potentialMoves) {
+                    ChessBoard clonedBoard = myBoard.clone();
+                    try {
+                        makeClonedMove(move, clonedBoard);
+                        if (!cloneIsInCheck(TeamColor.WHITE, clonedBoard)) {
+                            playableMoves.add(move);
                         }
+                    } catch (InvalidMoveException e) {
+                        throw new RuntimeException(e);
                     }
                 }
             }
         }
-
-        // an invalid move is any one which leaves the king in check.
-        // check each potential move of a piece. Move the piece to each position, and in each position check if the king is in check.
-        // if the king is in check, do not add that move to the list of valid moves.
-
-        //if the king is in check and cannot move and a piece is available to be moved in front of the king, then the only valid moves
-        //will be that which protects the king from check.
-
-        //Also limits the kings moves from placing itself in check.
-
         return playableMoves;
     }
 
@@ -124,12 +98,12 @@ public class ChessGame {
         if (pieceToMove == null) {
             throw new InvalidMoveException("No piece at start position");
         }
-        if (pieceToMove.getTeamColor() != teamTurn) {
-            throw new InvalidMoveException("It's not your turn.");
-        }
-        if (!validMoves(move.getStartPosition()).contains(move)) {
-            throw new InvalidMoveException("Invalid move for the piece");
-        }
+//        if (pieceToMove.getTeamColor() != teamTurn) {
+//            throw new InvalidMoveException("It's not your turn.");
+//        }
+//        if (!validMoves(move.getStartPosition()).contains(move)) {
+//            throw new InvalidMoveException("Invalid move for the piece");
+//        }
 
         ChessPosition endPosition = move.getEndPosition();
         clonedBoard.addPiece(endPosition, pieceToMove);
@@ -158,9 +132,9 @@ public class ChessGame {
         if (pieceToMove.getTeamColor() != teamTurn) {
             throw new InvalidMoveException("It's not your turn.");
         }
-        if (!validMoves(move.getStartPosition()).contains(move)) {
-            throw new InvalidMoveException("Invalid move for the piece");
-        }
+//        if (!validMoves(move.getStartPosition()).contains(move)) {
+//            throw new InvalidMoveException("Invalid move for the piece");
+//        }
 
         ChessPosition endPosition = move.getEndPosition();
         myBoard.addPiece(endPosition, pieceToMove);
@@ -168,6 +142,66 @@ public class ChessGame {
 
         teamTurn = (teamTurn == teamColor.WHITE) ? teamColor.BLACK : teamColor.WHITE;
 
+    }
+
+    public boolean cloneIsInCheck(TeamColor teamColor, ChessBoard clonedBoard) {
+        ChessPosition whiteKingPosition = null;
+        ChessPosition blackKingPosition = null;
+        for (int i = 1; i < 9; i++) {
+            for (int j = 1; j < 9; j++) {
+                ChessPosition myPosition = new ChessPosition(i,j);
+                ChessPiece myPiece = clonedBoard.getPiece(myPosition);
+                if (myPiece != null && myPiece.getPieceType() == KING) {
+                    if (myPiece.getTeamColor() == TeamColor.BLACK) {
+                        blackKingPosition = myPosition;
+                    }
+                    else {
+                        whiteKingPosition = myPosition;
+                    }
+                }
+            }
+        }
+        // Go through each of the opponent's valid moves' endpositions. If any of them correspond with the position of the king, then the king is in check.
+
+        if (teamColor == TeamColor.BLACK) {
+            for (int i = 1; i < 9; i++) {
+                for (int j = 1; j < 9; j++) {
+                    ChessPosition myPosition = new ChessPosition(i,j);
+                    ChessPiece myPiece = clonedBoard.getPiece(myPosition);
+                    if (myPiece != null) {
+                        if (myPiece.getTeamColor() == TeamColor.WHITE ) {
+                            Collection<ChessMove> potentialMoves = myPiece.pieceMoves(clonedBoard, myPosition);
+                            for (ChessMove move : potentialMoves) {
+                                if (move.getEndPosition().equals(blackKingPosition)) {
+                                    return true;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        else {
+            //CHECK IF WHITE IS IN CHECK
+            for (int i = 1; i < 9; i++) {
+                for (int j = 1; j < 9; j++) {
+                    ChessPosition myPosition = new ChessPosition(i,j);
+                    ChessPiece myPiece = clonedBoard.getPiece(myPosition);
+                    if (myPiece != null) {
+                        if (myPiece.getTeamColor() == TeamColor.BLACK ) {
+                            Collection<ChessMove> potentialMoves = myPiece.pieceMoves(clonedBoard, myPosition);
+                            for (ChessMove move : potentialMoves) {
+                                if (move.getEndPosition().equals(whiteKingPosition)) {
+                                    return true;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+        return false;
     }
 
     /**
@@ -201,7 +235,7 @@ public class ChessGame {
                     ChessPosition myPosition = new ChessPosition(i,j);
                     ChessPiece myPiece = myBoard.getPiece(myPosition);
                     if (myPiece != null) {
-                        if (myPiece.getTeamColor() == TeamColor.WHITE && myPiece.getPieceType() != KING) {
+                        if (myPiece.getTeamColor() == TeamColor.WHITE ) {
                             Collection<ChessMove> potentialMoves = myPiece.pieceMoves(myBoard, myPosition);
                             for (ChessMove move : potentialMoves) {
                                 if (move.getEndPosition().equals(blackKingPosition)) {
@@ -220,7 +254,7 @@ public class ChessGame {
                     ChessPosition myPosition = new ChessPosition(i,j);
                     ChessPiece myPiece = myBoard.getPiece(myPosition);
                     if (myPiece != null) {
-                        if (myPiece.getTeamColor() == TeamColor.BLACK && myPiece.getPieceType() != KING) {
+                        if (myPiece.getTeamColor() == TeamColor.BLACK ) {
                             Collection<ChessMove> potentialMoves = myPiece.pieceMoves(myBoard, myPosition);
                             for (ChessMove move : potentialMoves) {
                                 if (move.getEndPosition().equals(whiteKingPosition)) {
