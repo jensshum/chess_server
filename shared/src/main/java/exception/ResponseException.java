925ff34e19f8bdfd1c0855e127c999b5930e74b9
Jens Shumway <jensshum@gmail.com>
2024-02-27 17:02:14 -0700

    Response Exception fixed

--- shared/src/main/java/exception/ResponseException.java
+++ shared/src/main/java/exception/ResponseException.java
@@ -1,22 +1,8 @@
 package exception;
 
 public class ResponseException extends Exception {
-    final private int statusCode;
-    final private String codeMessage;
 
-    public ResponseException(int statusCode, String message) {
+    public ResponseException( String message) {
         super(message);
-        this.statusCode = statusCode;
-        this.codeMessage = message;
     }
-
-    public int StatusCode() {
-        return statusCode;
-    }
-
-    public String CodeMessage() {
-        return codeMessage;
-    }
-
-
 }
